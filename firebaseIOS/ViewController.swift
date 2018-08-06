8ba0944eafd6b5ca84860739986a62efefc8e881
androidworkspace <59022222@up.ac.th>
2018-08-06 10:42:12 +0700

    Initial Commit

---
+++ firebaseIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebaseIOS
+//
+//  Created by Android  on 6/8/2561 BE.
+//  Copyright © 2561 Android . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
